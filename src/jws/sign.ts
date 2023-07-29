3de0cf6e99216df1c1084713a335971a15b99be7
Aman <amanyadavlko2502@gmail.com>
2023-07-29 22:01:06 +0530

    Added jws/sign.ts

---
+++ src/jws/sign.ts
@@ -0,0 +1,32 @@
+
+import { Buffer } from "buffer";
+import {toString} from  "./toString";
+import * as util from "util";
+import {signPayload} from "jwa";
+
+function base64url(string: string, encoding: BufferEncoding) {
+  return Buffer.from(string, encoding)
+    .toString("base64")
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+}
+
+function jwsSecuredInput(header, payload, encoding) {
+  encoding = encoding || "utf8";
+  var encodedHeader = base64url(toString(header), "binary");
+  var encodedPayload = base64url(toString(payload), encoding);
+  return util.format("%s.%s", encodedHeader, encodedPayload);
+}
+
+function sign(opts) {
+  var header = opts.header;
+  var payload = opts.payload;
+  var encoding = opts.encoding;
+  var keyStore = opts.keyStore;
+  var securedInput = jwsSecuredInput(header, payload, encoding);
+  var signature = signPayload(securedInput, keyStore);
+  return util.format("%s.%s", securedInput, signature);
+}
+
+module.exports = { sign };
--- src/jws/toString.ts
+++ src/jws/toString.ts
@@ -1,7 +1,7 @@
 
 import { Buffer } from "buffer";
 
-module.exports = function toString(obj : object) {
+export function toString(obj : object) {
   if (typeof obj === "string") 
     return obj;
   if (typeof obj === "number" || Buffer.isBuffer(obj)) 
