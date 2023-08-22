58e00a6c5af60536215eed5794d704ad34736728
Aman <amanyadavlko2502@gmail.com>
2023-08-23 00:44:16 +0530

    Finished jwaVerify()

--- src/jwa/verify.ts
+++ src/jwa/verify.ts
@@ -1,15 +1,23 @@
 import type { Verifier } from "../types";
+import { JwaVerifyError } from "../errors";
+
 
 export async function jwaVerify(
-  verifier: Verifier,
+	verifierID: number,
+	verifier: Verifier,
   payload: string,
   signature: string,
-  address?: string
+  address: string
 ): Promise<boolean> {
-  if (!address)
-    return (await verifier(payload, signature, address)) as boolean;
-  else {
-    const addressRecovered = await verifier(payload, signature);
-    return addressRecovered === address;
+
+  switch(verifierID){
+    case 0 : {
+      return await verifier(payload, signature, address) as boolean;
+    };
+    case 1: {  
+			const addressRecovered : string = await verifier(payload, signature) as string;
+      return addressRecovered === address;
+    };
+    default: throw new JwaVerifyError("Incorrect Verifier function ID, can be only 0 or 1.", verifierID)
   }
 }
