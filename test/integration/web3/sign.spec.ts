7a7a1d054713903d3bf9954df5b841d85599ddd3
Aman <amanyadavlko2502@gmail.com>
2023-09-04 12:23:26 +0530

    Added first test

---
+++ test/integration/web3/sign.spec.ts
@@ -0,0 +1,18 @@
+import {web3Sign} from "../../sharedFixtures";
+import {sign} from "../../../src"
+import { expect, describe, it } from '@jest/globals';
+
+describe("Test for signing: sign()", () => {
+
+    it('sign the payload and get the token', async () => {
+        const token = await sign({
+            "nonce" : 654321,
+            "iat" : 1582062696,
+            "exp" : 1782098690,
+            "iss" : "0x231a5147b7c2bDF1dc8449Da0DeF741077447bCD"
+        }, web3Sign)
+
+        expect(token).not.toBe(void 0);
+        expect(typeof(token)).toBe("string");
+    });
+});
