970b3812ecf970f985d532af64eb59739a76094f
Aman <amanyadavlko2502@gmail.com>
2023-10-17 20:47:45 +0530

    Ethers examples setup

---
+++ examples/ethers/ethersSign.ts
@@ -0,0 +1,14 @@
+import * as ethers from "ethers";
+import fs from "fs";
+
+export async function signEthers(payloadAndHeader: string) {
+  const keystoreWallet = (fs.readFileSync('test/sharedFixtures/ethers/keystoreWallet.json')).toString();
+
+  const wallet = await ethers.Wallet.fromEncryptedJson(keystoreWallet, "pass_12345678");
+
+  let messageDigest = ethers.hashMessage(payloadAndHeader)
+  const signingKey = new ethers.SigningKey(wallet.privateKey);
+
+  const signature = signingKey.sign(messageDigest);
+  return signature.serialized;
+};
