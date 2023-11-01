57bc96fdde0bd71bb23e14854d79284aec44290d
Aman <amanyadavlko2502@gmail.com>
2023-11-02 00:22:52 +0530

    Added tests for sign() options update

--- test/integration/sign.spec.ts
+++ test/integration/sign.spec.ts
@@ -13,13 +13,11 @@ import type { Signer } from "../../src";
 describe("Test for signing: sign()", () => {
   it.each([
     [web3Sign, "0x231a5147b7c2bDF1dc8449Da0DeF741077447bCD", "ES256k"],
-    [signEthers, "0x145831eba8085d78c1d30A9C108aAD8A1501d6e0", "ES256k"],
     [
       signPolkadot,
       "5F7MBfGdyTg5th5gzsWMUyaVBRUkhEZw5Q82rPrtSP1q9F3E",
       "SR25519",
     ],
-    [signBitcoin, "1HZwtseQ9YoRteyAxzt6Zq43u3Re5JKPbk", "ES256k"],
     [metamaskSign, "0x29c76e6ad8f28bb1004902578fb108c507be341b", "ES256k"],
   ])(
     "sign with %p",
@@ -37,6 +35,39 @@ describe("Test for signing: sign()", () => {
         },
         signFunc,
         { algorithm }
+      );
+
+      expect(token).not.toBe(void 0);
+      expect(typeof token).toBe("string");
+      expect(token.split(".").length).toBe(3);
+    }
+  );
+
+
+  it.each([
+    [signEthers, "0x145831eba8085d78c1d30A9C108aAD8A1501d6e0", "ES256k"],
+    [
+      signPolkadot,
+      "5F7MBfGdyTg5th5gzsWMUyaVBRUkhEZw5Q82rPrtSP1q9F3E",
+      "SR25519",
+    ],
+    [signBitcoin, "1HZwtseQ9YoRteyAxzt6Zq43u3Re5JKPbk", "ES256k"],
+  ])(
+    "sign with %p with header in options instead of algorithm",
+    async (
+      signFunc: Signer,
+      address: string,
+      algorithm: string 
+    ) => {
+      const token = await sign(
+        {
+          nonce: 654321,
+          iat: 1582062696,
+          exp: 1782098690,
+          iss: address,
+        },
+        signFunc,
+        { header: {alg : algorithm} }
       );
 
       expect(token).not.toBe(void 0);
